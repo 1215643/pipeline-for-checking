f2fd9757f9178eb29ad77f17567f6ace7be83abd
1215643 <106243632+1215643@users.noreply.github.com>
2024-03-07 14:40:17 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,3 +1,11 @@
 def call() {
-sh 'mvn clean install'
+    try {
+        // Run Maven clean install
+        sh 'mvn clean install'
+    } catch (Exception e) {
+        // Handle any exceptions or errors that occur during the build process
+        echo "Maven build failed: ${e.message}"
+        currentBuild.result = 'FAILURE'
+    }
 }
+
