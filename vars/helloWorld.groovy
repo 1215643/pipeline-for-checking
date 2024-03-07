601f8310aad24e2781bd990a65f177b13cceba25
1215643 <106243632+1215643@users.noreply.github.com>
2024-03-07 11:33:05 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,7 @@
 def call() {
-sh 'echo hi from devops'
+sh 'Other content from aws.amazon.com
+AWS Management Console - AWS Management Console
+AWS Lambda | Serverless Computing - AWS Lambda - Amazon Web Se…
+Amazon EC2 - Secure and Resizable Cloud Compute – Amazon EC2 – …
+See more'
 }
